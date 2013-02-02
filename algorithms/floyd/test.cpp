9ea9da3a563f5af137701d6318c7408798fdf62f
Chad <chad.wyszynski@gmail.com>
2013-02-02 14:55:48 -0800

    Trivial test 1 (undirected)

--- algorithms/floyd/test.cpp
+++ algorithms/floyd/test.cpp
@@ -1,6 +1,23 @@
 #include "floyd.h"
 #include "../graph.hh"
+// undirected graph tests
+void undirected_tests(void); // runs all undirected graph tests
+void undirected_one_edge(void);
+
+// unit tests for undirected graphs
+void undirected_tests(void) {
+	undirected_one_edge();
+}
+
+// Tests floyd for one edge (trivial case)
+void undirected_one_edge(void) {
+	Graph g(1);
+	DistanceMatrix *asps = floyd(&g);
+
+	delete_distance_matrix(asps);
+}
 
 int main() {
+	undirected_tests();
 	return 0;
 }
