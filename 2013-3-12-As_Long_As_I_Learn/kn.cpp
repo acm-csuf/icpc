e4dd120e0b10df912bb7fa739e4bcfa49efb1406
Kiet Nguyen <kietnguyen86@gmail.com>
2013-03-19 22:51:47 -0700

    Update kn.cpp
    Easier readability

--- 2013-3-12-As_Long_As_I_Learn/kn.cpp
+++ 2013-3-12-As_Long_As_I_Learn/kn.cpp
@@ -10,34 +10,34 @@ using namespace std;
 typedef vector<int> vi;
 
 int main() {
-	vi w;												// learning units
-	vector<vi> adj;							// adjacent list
+	vi w;							// learning units
+	vector<vi> adj;						// adjacent list
 	
-	int t;											// # of test cases
+	int t;							// # of test cases
 	cin >> t;
 	
 	for (int i=0; i<t; i++) {
-		int n, m;									// n: # of nodes; m: # of edges
+		int n, m;					// n: # of nodes; m: # of edges
 		cin >> n >> m;
 		
-		int wt;										// wt: temp learning unit
+		int wt;						// wt: temp learning unit
 		for (int j=0; j<n; j++) {
 			cin >> wt;
 			w.push_back(wt);
 			adj.push_back(vi());
 		}
 				
-		int fr, to;								// fr: from node; to: to node
+		int fr, to;					// fr: from node; to: to node
 		for (int j=0; j<m; j++) {
 			cin >> fr >> to;
 			adj[fr].push_back(to);
 		}
 		
-		int totalL = 0;						// totalL: total learning
-		int curr = 0;							// curr: current checking node
+		int totalL = 0;					// totalL: total learning
+		int curr = 0;					// curr: current checking node
 		while (adj[curr].size() > 0) {
-			int maxL = 0;						// maxL: max learning units from 1 node
-			int maxP = -1;					// maxP: node that has that max 
+			int maxL = 0;				// maxL: max learning units from 1 node
+			int maxP = -1;				// maxP: node that has that max 
 			for (int j=0; j < (int)adj[curr].size(); j++) {
 				if (maxL < w[adj[curr][j]]) {
 					maxL = w[adj[curr][j]];
