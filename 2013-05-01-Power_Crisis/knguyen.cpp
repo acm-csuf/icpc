ac6016e669b7c0437d82c4aabcbd9dde6dd346c5
Kiet Nguyen <kietnguyen86@gmail.com>
2013-05-07 16:08:59 -0700

    Correct comment and add enum for readability

--- 2013-05-01-Power_Crisis/knguyen.cpp
+++ 2013-05-01-Power_Crisis/knguyen.cpp
@@ -8,6 +8,9 @@
 
 using namespace std;
 
+enum status {ALREADY_OFF = true, NOT_YET_OFF = false};
+enum region_name {AUCKLAND = 0, WELLINGTON = 12};
+
 int main (int argc, char const* argv[]) {
 	int N;	// # of regions
 	
@@ -17,22 +20,28 @@ int main (int argc, char const* argv[]) {
 		int m = -1;	
 		
 		for (unsigned int i = 1; i < N; i++) {
-			int count = N-1;	// # of regions that haven't been off yet
+			int count = N-1;	// # of remaining regions that haven't been off yet
 			int index = 0;	// index of which regions from 0 to N-1
-			vector<bool> off(N,false);	// to keep track which region is on (true) or off (false)
-			off[index] = true;	// always start from Auckland, which is 1
+			vector<bool> off(N, NOT_YET_OFF);	// to keep track which region is already off (true) or not-yet (false)
+			off[AUCKLAND] = ALREADY_OFF;	// always start from Auckland (region 1, index 0)
 			
-			while (count > 0 && index != 12) {
+			// keep turn off one region for each iteration until Wellington (region 13, index 12) 
+			// turn off early or all regions got turned off
+			while (count > 0 && index != WELLINGTON) {
 				int j=0;	// to count up to m
+				
+				// find next region to turn off by counting up m not-yet-off regions
 				do {
-					index = (index+1) % N;
+					index = (++index) % N;
 					j += (!off[index]);
 				} while (off[index] || (j<i));
 				
 				//printf("\t%d",index);
 
+				// decrease # of remaining regions
 				count--;
-				off[index] = true;
+				// set chosen region to already-off
+				off[index] = ALREADY_OFF;
 			}	
 			
 			//printf("\t%s\n", (count==0?"found":"not"));
