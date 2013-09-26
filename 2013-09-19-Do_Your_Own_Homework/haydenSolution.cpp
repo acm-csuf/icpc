457f586daef17d60acdf9af03c72b4803d789968
Hayden Donze <hldonze@gmail.com>
2013-09-26 14:39:02 -0700

    Added my solution to 9-19
    Solution for "Do Your Own Homework!" added.

---
+++ 2013-09-19-Do_Your_Own_Homework/haydenSolution.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Course
+{
+	string name;
+	int days;
+};
+
+int main()
+{
+	int T, N, to_due;
+	Course* courses;
+	string to_do;
+	bool flag = false;
+
+	cin >> T;
+	for (int i=0; i<T; i++)
+	{
+		cin >> N;
+		courses = new Course[N];
+		flag = false;
+		for (int j=0; j<N; j++)
+		{
+			cin >> courses[j].name;
+			cin >> courses[j].days;
+		}
+		cin >> to_due >> to_do;
+		for (int k=0; k<N; k++)
+		{
+			if (to_do == courses[k].name)
+			{
+				if (courses[k].days <= to_due)
+				{
+					cout << "Case " << (i+1) << ": Yesss\n";
+					flag = true;
+				}
+				else if (courses[k].days <= to_due + 5)
+				{
+					cout << "Case " << (i+1) << ": Late\n";
+					flag = true;
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+
+		if (!flag) cout << "Case " << (i+1) << ": Do your own homework!\n";
+	}
+
+	return 0;
+}
