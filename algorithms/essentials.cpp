c73dfd4dee89000e7c54d653794162461087ff40
baparker <parkerbrittany19@yahoo.com>
2014-02-13 13:47:38 -0800

    Redirection Operators Example
    Finished the example program and explanation for redirection operators

--- algorithms/essentials.cpp
+++ algorithms/essentials.cpp
@@ -1,65 +1,34 @@
-// This is NOT COMPLETE :)
-
-// example of string class
-// parse input
-// compile on command line
-// read input until EOF (end of file)
-// input/output redirect
+// ============================================================================
+// File: essentials.cpp
+// ============================================================================
+// Author: Brittany Parker
+// Date: 02/13/2014
+// Description:
+//		Explains input and output redirection using a simple program.
+//
+// Outline:
+//		Program - simple string class example
+//		Introduction
+//		Read_Input_Until_EOF
+//			Compile_In_Command_Line
+//		Input/Output_Redirecting
+//		Diff
+//		Parse_Input
+//
+// ============================================================================
 
 #include <iostream>
 #include <string>
 using namespace std;
 
-// == Compile_on_Command_Line =======================================
-// 
-// $ g++ myfile.cpp -o name
-// $ ./name
-// 
-// ==================================================================
-
-// == Read_input_until_(EOF) ========================================
-// 
-// If you are using the command line to enter input, this
-// program will wait for input. Once you type something and
-// press enter, the program will read each "word" in a loop
-// until it reaches the end of the line (where you pressed enter),
-// and it will print each "word" out onto the command line
-// followed by a new line.
-//
-// Example: you type "1 2 3", and you see
-// 1
-// 2
-// 3
-//
-// Then the program waits for you to enter something again.
-// If you press "cntrl + c" the program will end. If you press
-// "cntrl + d" the program also ends. The difference is that
-// "cntrl + d" represents the EOF character which is read by the
-// program and tells the while loop to stop looping, while "cntrl + c"
-// actually terminates the program no matter what it is doing.
-//
-// In short, the program is waiting for the EOF character to end,
-// even though you did not specify this in the program. This is why
-// the program stops when it is reading from an input file (instead
-// of reading from the command line).
-//
-// ==================================================================
-
-// == Input/Output_Redirecting ======================================
-//
-// The cool thing is that you can give the program a file without
-// using file I/O! That's right; no opening and closing files or
-// any extra lines of code in your program. How you do this is with
-// redirection operators ('<' and '>').
-//
-// In your command line
-//
-// ==================================================================
+// The following program is used for explanation:
 
 int main(void)
 {
 	string mystring;
-	
+
+	// Read input one word at a time.
+	// Stops looping when string contains EOF.
 	while(cin >> mystring)
 		{
 		cout << mystring << endl;
@@ -67,3 +36,69 @@ int main(void)
 
 	return 0;
 }
+
+// == Introduction ============================================================
+//
+// Here's something cool. You can give a program a file without using file I/O!
+// That's right; no opening and closing files or any extra lines of code in
+// your program. Let's look at the program above.
+//
+// == Read_Input_Until_EOF ====================================================
+//
+// If you are using the command line to enter input, this program will read to 
+// standard input (stdin) and standard output (stdout), which is the command
+// line. It waits for input from the stdin. Once you type something and press
+// enter, the program will read each "word" in a loop until it reaches the end
+// of the line (where you pressed enter), and it will print each "word" out to
+// stdout followed by a new line. For example,
+//
+// ---- Compile_In_Command_Line -----------
+//
+// $ g++ myfile.cpp -o name
+// $ ./name
+// >1 2 3
+// 1
+// 2
+// 3
+//
+// Then the program waits for you to enter something again. If you press
+// "cntrl + c" the program will end. If you press "cntrl + d" the program also
+// ends. The difference is that "cntrl + d" represents the EOF (end of file)
+// character which is read by the program and tells the while loop to stop
+// looping, while "cntrl + c" actually terminates the program no matter what it
+// is doing.
+//
+// In short, the program is waiting for the EOF character to end. This will
+// work great for files too even without explicit file I/O. This is because the
+// EOF character is at the end of every file.
+//
+// == Input/Output_Redirecting ================================================
+//
+// How you do this, is with redirection operators (i.e '<' and '>'). Let's see
+// an example:
+//
+// $ g++ myfile.cpp -o name
+// $ ./name < in.txt				// reads from in.txt; writes to stdout
+// $ ./name > out.txt				// reads from stdin; writes to out.txt
+// $ ./name < in.txt > out.txt 		// reads from in.txt; writes to out.txt
+// 
+// Your program simply needs to read input from stdin until it encounters
+// the EOF character.
+//
+// == Diff ====================================================================
+//
+// Another useful thing is quickly checking your output against correct output.
+// $ diff output.txt correctOutput.txt
+//
+// This lists all the differences between the files which is a really easy way
+// to check your work without having to do so manually/visually.
+//
+// == Parse_Input =============================================================
+// 
+// "parserinput.cpp" serves as a simple example of the following. Please see
+// "parsing_csv.cpp" and/or "stringparser.cpp" for further usage.
+//		strings
+//		stringstreams
+//		vectors
+//
+// ============================================================================
