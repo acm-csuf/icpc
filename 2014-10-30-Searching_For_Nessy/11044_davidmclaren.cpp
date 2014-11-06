09ad723283b9bf0f44ad94e03ddd7f3d00284f45
David McLaren <sirusblk@gmail.com>
2014-11-06 14:02:33 -0800

    Added David McLaren's accepted solution.

---
+++ 2014-10-30-Searching_For_Nessy/11044_davidmclaren.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	// Variables
+	int num_loop;
+	int row;
+	int col;
+	int adj1;
+	int adj2;
+
+	// Get Number of Loops
+	cin >> num_loop;
+
+	// Loop
+	for (int i = 0; i < num_loop; ++i)
+	{
+		cin >> row >> col;
+
+		// Equation
+		// Round to nearest int
+		adj1 = (int) (row + 0.5) / 3;
+		adj2 = (int) (col + 0.5) / 3;
+
+		// Return answer
+		cout << adj1 * adj2 << endl;
+	}
+
+	return 0;
+}
